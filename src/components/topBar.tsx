b3b4bf02343b71119b8a6d0034808dbfb895f266
nguyentrucanhtuan <nguyentrucanhtuan@gmail.com>
2023-12-09 20:06:33 +0700

    cap nhat topBar

--- src/components/topBar.tsx
+++ src/components/topBar.tsx
@@ -5,24 +5,90 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import { Divider, Drawer, List, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
+import InboxIcon from '@mui/icons-material/Inbox';
+import DraftsIcon from '@mui/icons-material/Drafts';
 
 export default function TopBar() {
+
+  const [openDrawer, setOpenDrawer] = React.useState(false);
+
+  function toggleDrawer(newOpen: boolean) {
+    setOpenDrawer(newOpen);
+  }
+
   return (
     <>
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="fixed">
-        <Toolbar variant="dense">
-          <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" color="inherit" component="div">
-            Cà Phê CoffeeTree
-          </Typography>
-        </Toolbar>
-      </AppBar>
-    </Box>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="fixed">
+          <Toolbar variant="dense">
+            <IconButton onClick={() => {toggleDrawer(true)}} edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" color="inherit" component="div">
+              Cà Phê CoffeeTree
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </Box>
+      
+      <Drawer
+        anchor="left"
+        open={openDrawer}
+        onClose={() => toggleDrawer(false)}
+      >
+        <Box
+          sx={{width: "250px"}}
+        >
+          <List>
+            <ListItem disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  <InboxIcon />
+                </ListItemIcon>
+                <ListItemText primary="Cà phê" />
+              </ListItemButton>
+            </ListItem>
+            
+            <ListItem disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  <DraftsIcon />
+                </ListItemIcon>
+                <ListItemText primary="Trà" />
+              </ListItemButton>
+            </ListItem>
 
-    
+            <ListItem disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  <DraftsIcon />
+                </ListItemIcon>
+                <ListItemText primary="Máy pha cà phê" />
+              </ListItemButton>
+            </ListItem>
+
+            <ListItem disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  <DraftsIcon />
+                </ListItemIcon>
+                <ListItemText primary="Sản phẩm bán chạy" />
+              </ListItemButton>
+            </ListItem>
+            <Divider />
+            <ListItem disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  <DraftsIcon />
+                </ListItemIcon>
+                <ListItemText primary="Thông tin khuyến mãi" />
+              </ListItemButton>
+            </ListItem>
+
+          </List>
+        </Box>
+      </Drawer>
     </>
   );
 }
